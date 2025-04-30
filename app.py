b083e8df8dd1bdc044df35311f6970811f62fb9c
rebornmeatstick <xsarkus@gmail.com>
2025-04-30 16:31:02 -0700

    gui improvements -mz

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-## =============== TODO LIST =================== ##
-## - Fix the axis labels on graphs one and two - ##
-## -             GUI improvement               - ##
-## -          Fix font (comic sans?)           - ##
-## ============================================= ##
+## v0.3 changelog
+## -- fixed the fonts (yay css!)
+## -- did a LOT of gui work (moving background!!!!)
+
+## todo
+## there's a bug with the gui where the css stuff doesn't load in time with the graphs, and the margins break and blah blah... I HATE GUI >:[ // ps - i gave up on the margin. no more bottom margin.
 
 ## IMPORT DEPENDENCIES ##
 from dash import dash, dcc, html, Input, Output, callback, ctx
@@ -15,23 +16,14 @@ import io
 ## ------------------- ##
 
 ## File reading code
-path = "C:/Users/caste/Desktop/jupyter_workingdirs/bme_finalProject/data/covid19data.csv" ## TODO: pleas find out how to use the working directory to access the data file so we don't keep accessing it manually 😭😭
+sourcepath = os.path.dirname(os.path.abspath(__file__))
+path = os.path.join(sourcepath, "data", "covid19data.csv") ## had to use os because the python file wasn't starting in the source folder for some reason??? guess this is more thorough though
 data = pd.read_csv(path, thousands=",") ## Reads the .csv
 data["collection_date"] = pd.to_datetime(data["collection_date"], format="%Y-%m-%d") ## Converts the collection dates to the format
 data.sort_values("collection_date", inplace=True) ## Sorts the dates chronologically
 
-
-## IMPORT FREDOKA FONT || Sidenote: this doesn't work? help? can we replace this with comic sans? :[ ##
-external_stylesheets = [
-    {
-        "href": "https://fonts.googleapis.com/css2?family=Fredoka"
-        "family=Lato:wght@300..700&display=swap",
-        "rel": "stylesheet",
-    },
-]
-
 ## App properties
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__)
 server = app.server
 app.title = "Analysis Project"
 
@@ -44,16 +36,16 @@ app.layout = html.Div(
             html.Center(
                 children=[
                     html.P(
-                        children="Version 0.0.2", className="headerTitle"
+                        children="Version 0.3", className="title"
                     ),
                     html.P(
-                        children="me when queen bee infects and kills all my employees during the qliphoth meltdown", className="headerTitle"
+                        children='i will now cook the "berried delight" - vergil', className="title"
                     ),
                     html.H1(
-                        children="Data Analysis Project of Doom and Despair", className="header"
+                        children="Data Analysis Project of Love and Prosperity", className="mainTitle"
                     ),
                     html.P(
-                        children="===============================================", className="headerTitle"
+                        children="===============================================", className="title"
                     ),
                 ],
                 className="header"
@@ -75,6 +67,7 @@ app.layout = html.Div(
                     ),
                 ],
             ),
+            className = 'dateWrapper'
         ),
         ## Graph Parameter Container
         html.Div(
@@ -115,10 +108,7 @@ app.layout = html.Div(
                             ],
                         ),
                     ],
-                    style={
-                        'flex': 1,
-                        'padding': '10px'
-                    },
+                    className = 'paramWrapper'
                 ),
                 ## Graph Two Parameter Div Container
                 html.Div(
@@ -155,18 +145,11 @@ app.layout = html.Div(
                                 ),
                             ],
                         ),
-
                     ],
-                    style={
-                        'flex': 1,
-                        'padding': '10px'
-                    },
+                    className = 'paramWrapper'
                 ),            
             ],
-            style={
-                'display': 'flex',
-                'justifyContent': 'space-between'
-            },   
+            className = 'wrapper'
         ),
         ## Graph Containers
         html.Div(
@@ -174,31 +157,19 @@ app.layout = html.Div(
                 html.Div(
                     children=dcc.Graph(
                         id="GraphOne",
-                        config={"displayModeBar": False},
+                        config={"displayModeBar": False, "responsive": True},
                     ),
                     className="card",
-                    style={
-                        'flex': 1,
-                        'padding': '10px'
-                    },
                 ),
                 html.Div(
                     children=dcc.Graph(
                         id="GraphTwo",
-                        config={"displayModeBar": False},
+                        config={"displayModeBar": False, "responsive": True},
                     ),
                     className="card",
-                    style={
-                        'flex': 1,
-                        'padding': '10px'
-                    },
                 ),
             ],
             className="wrapper",  
-            style={
-                'display': 'flex',
-                'justifyContent': 'space-between'
-            },  
         ),
         ## Save Button Container
         html.Div(
@@ -234,6 +205,8 @@ app.layout = html.Div(
 )
 ## Main app callback function
 def update(state1, attribute1, state2, attribute2, startDate, endDate, n_clicks):
+    plot_rgbcolor = 'rgba(255, 250, 240,1)'
+    paper_rgbcolor = 'rgba(255, 250, 240,1 )'
     ## Filter Dataset One for graph one
     mask1 = (
         (data.state.isin(state1))
@@ -255,26 +228,49 @@ def update(state1, attribute1, state2, attribute2, startDate, endDate, n_clicks)
         data_frame = filteredData1,
         x = filteredData1.collection_date,
         y = np.sort(filteredData1[str(attribute1)].apply(pd.to_numeric)),
-        color=filteredData1["state"],
-        labels={'collection_date': "Date", 'y': attribute1},
-        title=f'Graph One: {attribute1} from {startDate} to {endDate}.'
+        color = filteredData1["state"],
+        labels = {'collection_date': "Date", 'y': attribute1},
+        title = f'Graph One: {attribute1} from {startDate} to {endDate}.',
     )
+
+    ## Make graph one transparent
+    chartOne.update_layout({
+        'plot_bgcolor': plot_rgbcolor,
+        'paper_bgcolor': paper_rgbcolor,
+    })
 
     chartTwo = px.line(
         data_frame = filteredData2,
         x = filteredData2.collection_date,
         y = np.sort(filteredData2[str(attribute2)].apply(pd.to_numeric)),  
-        color=filteredData2["state"] ,
-        labels={'collection_date': "Date", 'y': attribute2},
-        title=f'Graph Two: {attribute2} from {startDate} to {endDate}.'
+        color = filteredData2["state"] ,
+        labels = {'collection_date': "Date", 'y': attribute2},
+        title = f'Graph Two: {attribute2} from {startDate} to {endDate}.',
     )
 
+    ## Make graph two transparent
+    chartTwo.update_layout({
+        'plot_bgcolor': plot_rgbcolor,
+        'paper_bgcolor': paper_rgbcolor,
+    })
+
     ## i don't like this code that i wrote! maybe we can split it into different callback? idk! i tried and it hurt my head!
+    ## also the graph layout update thing is pmo ... wanna change but it works for now so ...
     ## i don't remember how to make it reference the graph outside of this function!!!! so for now, this hack works :)
 
     ## Downloading code for downloading the files that you want to download onto your computer that you're using to download the files
     if ctx.triggered_id == "download_graphs":
         downloadDate = datetime.datetime.now().strftime("%H:%M:%S")
+
+        chartOne.update_layout({
+        'plot_bgcolor': 'rgba(204, 204, 255,1)',
+        'paper_bgcolor': 'rgba(255,255,255,1)',
+        })
+
+        chartTwo.update_layout({
+        'plot_bgcolor': 'rgba(204, 204, 255,1)',
+        'paper_bgcolor': 'rgba(255,255,255,1)',
+        })
 
         ## saves the image byte data into a buffer. pretty sure it's not necessary, but i don't want to change it because this works!
         buffer1 = io.BytesIO(chartOne.to_image(format="png"))
@@ -282,6 +278,16 @@ def update(state1, attribute1, state2, attribute2, startDate, endDate, n_clicks)
 
         save1 = dcc.send_bytes(src = buffer1.getvalue(), filename = f"GraphOne_{downloadDate}.png")
         save2 = dcc.send_bytes(src = buffer2.getvalue(), filename = f"GraphTwo_{downloadDate}.png")
+
+        chartOne.update_layout({
+        'plot_bgcolor': plot_rgbcolor,
+        'paper_bgcolor': paper_rgbcolor,
+        })
+
+        chartTwo.update_layout({
+        'plot_bgcolor': plot_rgbcolor,
+        'paper_bgcolor': paper_rgbcolor,
+        })
 
         return chartOne, chartTwo, save1, save2
     else:
